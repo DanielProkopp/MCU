1fe0274f3d7d2fb92e083500c5b9354865670273
Daniel Prokopp <danielprokopp2@gmail.com>
2018-05-19 22:32:25 -0300

    Versão final antes dos testes

---
+++ doc/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,4 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "lib.h", "lib_8h_source.html", null ]
+];
---
+++ doc/html/navtreedata.js
@@ -0,0 +1,16 @@
+var NAVTREE =
+[
+  [ "Projeto Seguidor de Linha", "index.html", [
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
